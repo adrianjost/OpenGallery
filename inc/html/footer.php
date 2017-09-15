a4d6484fb31dc077f09ac6451581f9fc5995d472
adrianjost <homepage@hackedit.de>
2017-09-15 22:01:10 +0200

    remove everything

--- inc/html/footer.php
+++
@@ -1,2 +0,0 @@
-<p style="text-align:center;color:#555;padding:5px;font-size:.6rem;">&copy; Copyright <a href="https://adrianjost.hackedit.de" rel="nofollow" style="color:#000";>Adrian Jost</a></p>
-</body></html>
